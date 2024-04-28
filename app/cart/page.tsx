89f7af6ecc5552ab96c97b290fb5008e6d5754aa
Stupel <hellomariandev@gmail.com>
2024-04-28 14:57:36 +0200

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -16,13 +16,13 @@ export default function CartPage() {
         </h1>
 
         <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
-          <section aria-labelledby="cart-heading" className="lg:col-span-7">
+          <div aria-labelledby="cart-heading" className="lg:col-span-7">
             <h2 id="cart-heading" className="sr-only">
               Items in your shopping cart
             </h2>
             {!cart.length && <CartItemsEmpty />}
             {!!cart.length && <CartItems />}
-          </section>
+          </div>
           <CartSummary />
         </div>
       </div>
