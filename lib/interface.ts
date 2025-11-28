919c545001e07d05e3a9638ff61d06227ae47613
Marián Hollý <marian.holly7117@gmail.com>
2025-11-28 17:03:18 +0100

    update interface

--- lib/interface.ts
+++ lib/interface.ts
@@ -10,7 +10,7 @@ export interface Product {
   name: string;
   author: string;
   price: number;
-  quantity?: number; // Used in cart
+  quantity: number; // Always set in cart context
   image: Image;
   body?: PortableTextBlock[];
   description?: string;
